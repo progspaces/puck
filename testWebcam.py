5741dabc5c10fc1d4c7144b46bc71c07160b22b9
julia <julia.dreiling@outlook.com>
2025-11-19 14:49:44 +0000

    slight updates

--- testWebcam.py
+++ testWebcam.py
@@ -1,23 +1,33 @@
 import cv2
-## Grabbed from https://stackoverflow.com/questions/2601194/displaying-a-webcam-feed-using-opencv-and-python
 
+imageOrVideo = "image"
 
-cv2.namedWindow("preview")
-vc = cv2.VideoCapture(0) # input index is 0, so first video input I assume 
-# returns a viedo capture object called vc
+def camera(grabFrames, grabFrameRate, grabFrameRun, imgOrVid):
+    ## Grabbed from https://stackoverflow.com/questions/2601194/displaying-a-webcam-feed-using-opencv-and-python
+    vc = cv2.VideoCapture(0) # input index is 0, so first video input I assume 
+    # returns a viedo capture object called vc
 
-if vc.isOpened(): # try to get the first frame
-    rval, frame = vc.read() # returns a value (boolean I think? true) and frame
-else:
-    rval = False
+    if vc.isOpened(): # try to get the first frame
+        rval, frame = vc.read() # returns a value (boolean I think? true) and frame
+        if imgOrVid == "image":
+            cv2.imwrite(f"frames/run{grabFrameRun}snapshot.jpg", frame)
+        else:
+            count = 0
+            while rval:
+                cv2.imshow("preview", frame)
+                rval, frame = vc.read() # returns the retreval value, and then. the image 
+                if count % grabFrameRate == 0 and grabFrames == True:
+                    print("blahs")
+                    cv2.imwrite(f"frames/run{grabFrameRun}frame{count}.jpg", frame)
+                count +=1
+                # combines grab and retrive in one call. if nothing grabbed, returns false.
+                key = cv2.waitKey(20)
+                if key == 27: # exit on ESC
+                    break
+    else:
+        rval = False
 
-while rval:
-    cv2.imshow("preview", frame)
-    rval, frame = vc.read() # returns the retreval value, and then. the image 
-    # combines grab and retrive in one call. if nothing grabbed, returns false.
-    key = cv2.waitKey(20)
-    if key == 27: # exit on ESC
-        break
+    cv2.destroyWindow("preview")
+    vc.release()    
 
-cv2.destroyWindow("preview")
-vc.release()
+camera(False,100,1, imageOrVideo)
