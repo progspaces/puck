5b006f0dfd69c004126fff9b54b9cf2e7da3fce0
julia <julia.dreiling@outlook.com>
2026-03-24 14:14:20 +0000

    updated experiments to be in line w old runs

--- puck/experiments/best_thresholding/binary_otsu.py
+++ puck/experiments/best_thresholding/binary_otsu.py
@@ -1,155 +1,131 @@
 import itertools
-from concurrent.futures import ThreadPoolExecutor
-from puck.experiments.best_thresholding.cartesian_product import generator
+from pipelineGenerator import generator
 from pathlib import Path
 import cv2 as cv
 from matplotlib import pyplot as plt
 import numpy as np
 import json
 import math
-import pprint
-import pandas as pd
-from time import thread_time_ns
-from datetime import datetime
-import statistics
-from tqdm import tqdm
+
 
 def blobParamFunc(minArea, minCircularity):
     params = cv.SimpleBlobDetector_Params()
-    params.filterByCircularity = True
-    params.minCircularity = minCircularity
-    params.minArea=minArea
+    # params.filterByCircularity = True
+    # params.minCircularity = minCircularity
+    # params.minArea=minArea
     params.blobColor = 0
     return params
-def detection(thresholds,blob_params,gray):
-    detector = cv.SimpleBlobDetector_create(blob_params)
-    centers= []
-    for thresh in thresholds:
-        _, thresholded = cv.threshold(gray, thresh, 255, cv.THRESH_BINARY)
-        keypoints = detector.detect(thresholded)
-        current_centers = [(round(kp.pt[0]), round(kp.pt[1])) for kp in keypoints]
-        new_centers = []
-        for curr_center in current_centers:
-            isNew = True
-            for c in centers:
-                    distance = math.dist(curr_center, c)
-                    isNew = distance >= 10
-                    if not isNew:
-                        break
-            if isNew:
-                new_centers.append(curr_center)
-        centers = centers + new_centers
-    return centers
-
 
 def groundTruthKeyPoints(entry):
     return [tuple(point[1:3]) for point in entry]
 
-def pipelineTests(args):
-    test_pipeline = args
-    results_path = Path("src/puck/dotpipeline/pipeline_results/" + str(choice))
-    results_path.mkdir(parents=True, exist_ok=True)
-    correct = 0 
-    times = []
-    pipeline_img_dict = {}
-    for img_path in sorted(list(p.glob('images/*/*/*/*/*[0-4].jpg'))):
-        # print(str(img_path))
-        gtkp = groundTruthKeyPoints(file_data.get(str(img_path)))
-        start= thread_time_ns()
-        image = cv.imread(img_path, cv.IMREAD_COLOR_RGB)
-        imageBlurred = cv.medianBlur(image, 9)
-        gray = cv.cvtColor(imageBlurred, cv.COLOR_RGBA2GRAY)
-        thresholds = [160,170,180]
-        blob_params = blobParamFunc(500, .8)
-        point_list = detection(thresholds=thresholds, blob_params= blob_params, gray=gray)
-        end = thread_time_ns()
-        distances = []
-        points_to_purge= []
-        for a, b in itertools.combinations(point_list, 2):
-            dist = (math.dist(a, b))
-            if dist<=3:
-                points_to_purge.append(a)
-        point_list = list(set(point_list) - set(points_to_purge))
 
-        for point in point_list:
-            closest = (100000, (0,0))
-            for gt in gtkp:
-                distance = math.dist(gt, point)
-                if distance < closest[0]:
-                    closest= (distance, gt,point)
-            distances.append(closest[0])
-        distances.sort()
-        first_four = distances[:4] if len(distances) >= 4 else distances
-        # variable list
-        
-        count_of_blobs = len(point_list)
-        only_4 = len(point_list) == 4
-        close_enough = True if all([(dist < 5) for dist in first_four]) else False
-        only_4_close_enough = only_4 and close_enough
-        if only_4_close_enough:
-            correct += 1
-        timeToDetect = end - start
-        times.append(timeToDetect)
-        pipeline_img_dict.update({img_path: (count_of_blobs, close_enough, only_4_close_enough, timeToDetect,distances)})
-        # # print("...")
-    # Create subdirectory for choice results
-    choice_results_path = results_path / str(choice)
-    choice_results_path.mkdir(parents=True, exist_ok=True)
-    pd.DataFrame(pipeline_img_dict).T.to_csv(choice_results_path / (str(test_pipeline) + "_results.csv"))
-    accuracy = (correct/480)
-    avgTimeToDetect = statistics.mean(times)
-    medianTimeToDetect = statistics.median(times)
-    return str(test_pipeline), (choice, accuracy, avgTimeToDetect, medianTimeToDetect)
-
-
-
-
-overall_start = thread_time_ns()
-
-# Open the ground truth annotations
 with open('./src/puck/cli/annotations.json' , "r") as json_file:
     file_data = dict(json.loads(json_file.read()))
 
-# Get the pipeline configuration options
 
-# Manually set this, (choice choice_time), so 0-3, and then 0 initially for choice_time, reset at the end of each for loop.
-choices = [("paperPrograms",0)]
+pipelines = ["src/puck/dotpipeline/binary0.json","src/puck/dotpipeline/adaptiveM0.json","src/puck/dotpipeline/adaptiveG0.json", "src/puck/dotpipeline/otsu0.json"]
+choice = int(input("Please enter 0-3 to choose binary(0) or adaptiveM(1) or adaptiveG(2) or otsu(3): "))
+pipeline_list = (generator(pipelines[choice]))[1]
+test_pipeline = pipeline_list[0]
+print(test_pipeline)
+
 p = Path('.')
+# for test_pipeline in pipeline_list:
+correct = 0 
+# for img_path in sorted(list(p.glob('images/*/*/*/*/*[0].jpg'))):
+for x in range(1,2,1):
+    img_path = "images/custom/davids/short/B/custom_davids_short_B_0.jpg"
+    gtkp = groundTruthKeyPoints(file_data.get(str(img_path)))
+    image = cv.imread(img_path, cv.IMREAD_COLOR_RGB)
+    imageBlurred = cv.medianBlur(image, test_pipeline[0])
+    thresholded =0
+
+    if choice == 0 : # binary
+        gray = cv.cvtColor(imageBlurred, cv.COLOR_RGBA2GRAY)
+        _, thresholded = cv.threshold(gray, test_pipeline[3], 255, cv.THRESH_BINARY)
+        print(thresholded.dtype)
+        plt.imshow(thresholded, cmap="gray")
+        plt.show()
+    elif choice == 1: ## Adaptive Mean
+        gray = cv.cvtColor(imageBlurred, cv.COLOR_RGBA2GRAY)
+        # thresholded = cv.adaptiveThreshold(gray,255,cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY,test_pipeline[3],test_pipeline[4])
+        thresholded = cv.adaptiveThreshold(gray,255,cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY,11,2)
+        thresholded = cv.fastNlMeansDenoising(thresholded)
+        print("THRESHOLDED")
+        plt.imshow(thresholded, cmap="gray")
+        plt.show()
+    elif choice == 2: ## Adaptive Gaussian
+        gray = cv.cvtColor(imageBlurred, cv.COLOR_RGBA2GRAY)
+        # thresholded = cv.adaptiveThreshold(gray,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY,test_pipeline[3],test_pipeline[4])
+        thresholded = cv.adaptiveThreshold(gray,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY,11,2)
+        plt.imshow(thresholded, cmap="gray")
+        plt.show()
+        print("THRESHOLDED")
+    else: 
+        pass
+        # convert the image into the hsv colour space
+        image_hsv = cv.cvtColor(imageBlurred, cv.COLOR_RGB2HSV)
+        image_hsv = imageBlurred
+ 
+        # use Otsu's method to find the thresholds for hue and saturation
+        _, thresh_h = cv.threshold(image_hsv[:, :, 0],0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+        _, thresh_s = cv.threshold(image_hsv[:, :, 1],0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+ 
+        # mask the image to get determine which pixels with hue and saturation above their thresholds
+        mask_h = image_hsv[:, :, 1] > thresh_h
+        mask_s = image_hsv[:, :, 1] > thresh_s
+ 
+        # combine the masks with an OR so any pixel above either threshold counts as foreground
+        np_mask = np.logical_or(mask_h, mask_s)
+ 
+        # apply morphological transforms
+        # for mt in self.morph_transform:
+        kernel = np.ones((3, 3), np.uint8)
+        thresholded = cv.morphologyEx(np_mask.astype(np.uint8), cv.MORPH_CLOSE, kernel)
+        # thresholded = 255-thresholded
+        plt.imshow(thresholded, cmap="gray")
+        plt.show()
+        print("THRESHOLDED")
+
+        
 
 
-for choice, choice_time in choices:
-    choice_start = thread_time_ns()
-    pipeline_list_dict = {}
-    with ThreadPoolExecutor(10) as pool:
-        # call a function on each item in a list and handle results
-        generatored_pipelines = generator("/Users/jdreiling/Desktop/puck/puck/src/puck/dotpipeline/binary1.json")[1]
-        for name, result in tqdm(pool.map(pipelineTests, generatored_pipelines), total=len(generatored_pipelines)):
-            pipeline_list_dict.update({name:result})
+    blob_params = blobParamFunc(test_pipeline[1], test_pipeline[2])
+    detector = cv.SimpleBlobDetector_create()
+    keypoints = detector.detect(thresholded)
+    print(str(len(keypoints)) + " blobs detected")
 
-    # ensure that output directory exists for the big picture
-    output_dir = Path("src/puck/dotpipeline/big_picture/")        
-    output_dir.mkdir(parents=True, exist_ok=True)
+    circles = cv.HoughCircles(thresholded, cv.HOUGH_GRADIENT,dp=1,minDist=2,maxRadius =1000)
+    print("circles")
+    print(circles)
 
-    # save the big picture results
-    pd.DataFrame(pipeline_list_dict).T.to_csv(output_dir / "paperProgramsThresholding_results.csv")
-    choice_end =thread_time_ns()
-    choice_time = choice_end - choice_start
-    
+    contours, hierarchy = cv.findContours(thresholded, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    print('contours')
+    print(contours)
+    cv.drawContours(imageBlurred, contours, -1, (0,255,0), 10)
+    plt.imshow(imageBlurred, cmap="gray")
+    plt.show()
 
-overall_end = thread_time_ns()
-overall_time = overall_end - overall_start
+    blank = np.zeros((1, 1))
+    blobs = cv.drawKeypoints(image, keypoints, blank, (255, 0, 0), cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    accuracy = []
+    for kp in keypoints:
+        closest = (100000, (0,0))
+        point = (kp.pt)
+        for gt in gtkp:
+            distance = math.dist(gt, point)
+            if distance < closest[0]:
+                closest= (distance, gt)
+        accuracy.append(closest[0])
+    print(accuracy)
+    if accuracy < [5,5,5,5] and len(accuracy) == 4:
+        print(accuracy)
+        correct +=1
+    # plt.imshow(blobs, interpolation="nearest")
+    # plt.show()
+print(correct)
 
-txtStr = f"This pipeline runner took {overall_time} time in total. \n" 
-for option, option_time in choices:
-    txtStr = txtStr + f"It spent {option_time} time on choice {option}. \n"
+## blob will be the same across the board
 
-# Ensure timing results directory exists
-timing_dir = Path('./src/puck/dotpipeline/')
-timing_dir.mkdir(parents=True, exist_ok=True)
 
-# Create timing results file with properly formatted datetime
-timing_filename = timing_dir / f"timingResults_{datetime.now().strftime('%Y%m%d_%H%M%S')}.txt"
-with open(timing_filename, "w") as txt_file:
-    txt_file.write(txtStr)
-    
-   
