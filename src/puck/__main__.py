c8900a21359b7c49b4b948a85df6f3ef906c1f33
Michael Young <mct25@st-andrews.ac.uk>
2026-09-18 15:05:34 +0100

    Add actors and more setup

--- src/puck/__main__.py
+++ src/puck/__main__.py
@@ -2,12 +2,19 @@
 from tkinter import * # TODO: avoid *
 base = Tk()
 
-# Other imports
+# Standard packages
 import logging
+
+# External packages
 import typer
+
+# Local packages and modules
+from actor import Actor
 
 # Global variables
 logger = logging.getLogger(__name__)
+PROGRAM_LOOKUP_FILE = 'puck/program_store/program_lookup.json' # TODO
+DICT = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_APRILTAG_16H5)
 
 def logging_setup(log: bool, log_level: int) -> None:
     if log:
@@ -24,10 +31,17 @@ def tk_setup() -> None:
     base.geometry("1920x1080+0+-1080")
 
 
+def load_program_store(filename: str) -> dict[str, str]:
+    with open(filename) as f:
+        return dict(load(f))
+
+
 def start_puck(log: bool = False, log_level: int = 0) -> None:
     print("Hello from puck!")
     logging_setup(log, log_level)
     tk_setup()
+    program_lookup = load_program_store(PROGRAM_LOOKUP_FILE)
+    encoding_to_actor: dict[str, Actor] = {}
 
 
 def main() -> None:
